1cdefa30694a82bcd677a2e62a8a902b8b088f6a
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 15:05:30 +0600

    65-5 Load And Display Data From Database, Create Delete Button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
+  const users = useLoaderData();
+
+  const handleDelete = (user) => {
+    const agree = window.confirm(
+      `Are You Sure, You want to delete: ${user.name}`
+    );
+    if (agree) {
+      console.log(`deleting user with ${user.name} id`);
+    }
+  };
+
   return (
     <div>
-      <h2>Users :</h2>
+      <h2>Users :{users.length}</h2>
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} {user.email}
+            <button onClick={() => handleDelete(user)}>X</button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
