8c2555be65ec47cbc47f7bf6c0696b600f352e08
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 16:33:52 +0600

    65-6 Delete A User From The Database And Show Remaining Users

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
 
   const handleDelete = (user) => {
     const agree = window.confirm(
       `Are You Sure, You want to delete: ${user.name}`
     );
     if (agree) {
-      console.log(`deleting user with ${user.name} id`);
+      //   console.log(`deleting user with ${user.name} id`);
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data)
+          if (data.deletedCount > 0) {
+            alert("User deleted successfully.");
+            const remainingUsers = displayUsers.filter(
+              (usr) => usr._id !== user._id
+            );
+            setDisplayUsers(remainingUsers);
+          }
+        });
     }
   };
 
