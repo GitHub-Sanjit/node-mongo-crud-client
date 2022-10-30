e95bdc0814ae9241e25f358ff65904048fa01c94
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 14:17:49 +0600

    65-4 Save Data From React Client Side To Mongo Database

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -6,6 +6,21 @@ const AddUser = () => {
   const handleAddUser = (event) => {
     event.preventDefault();
     console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert("User Added Successfully");
+          event.target.reset();
+        }
+      });
   };
 
   const handleInputBlur = (event) => {
